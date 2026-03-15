7e7fde2d593dd8236018a96c194c11d0c866236d
seajihey <okcoco03@naver.com>
2026-03-15 15:50:58 +0900

    feat:webVM

--- src/main/java/com/vmportal/repository/ProvisionRequestRepository.java
+++ src/main/java/com/vmportal/repository/ProvisionRequestRepository.java
@@ -4,4 +4,5 @@ import com.vmportal.entity.ProvisionRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProvisionRequestRepository extends JpaRepository<ProvisionRequest, Long> {
+//yes
 }
